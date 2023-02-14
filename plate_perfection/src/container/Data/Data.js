809eb6db5c087adabd173aaf1b21c92197697387
Moodyean <andrefnmatos@gmail.com>
2023-02-14 20:39:00 +0000

    folder data

---
+++ plate_perfection/src/container/Data/Data.js
@@ -0,0 +1,278 @@
+const productData = [
+    {
+        name: "Culinary Compass",
+        id: 1,
+        category: "",
+        image: `${images.plate1}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingrdient 3",
+        }]
+    },
+    {
+        name: "Culinary Compass",
+        id: 2,
+        category: "",
+        image: `${images.plate2}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },{
+        name: "Culinary Compass",
+        id: 3,
+        category: "",
+        image: `${images.plate3}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },
+    {
+        name: "Culinary Compass",
+        id: 4,
+        category: "",
+        image: `${images.plate1}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },
+    {
+        name: "Culinary Compass",
+        id: 5,
+        category: "",
+        image: `${images.plate2}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },{
+        name: "Culinary Compass",
+        id: 6,
+        category: "",
+        image: `${images.plate3}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },{
+        name: "Culinary Compass",
+        id: 7,
+        category: "",
+        image: `${images.plate1}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },
+    {
+        name: "Culinary Compass",
+        id: 8,
+        category: "",
+        image: `${images.plate2}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },{
+        name: "Beautiful Salad",
+        id: 9,
+        category: "",
+        image: `${images.plate3}`,
+        description: "Descriptive text to entice the client to click",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        price: "12,99€",
+        extras: [
+            {
+                name: "Extra 1",
+                price: "3.99€",
+                id: 1,
+            },
+            {
+                name: "Extra 2",
+                price: "2.99€",
+                id: 2,
+            },
+            {
+                name: "Extra 3",
+                price: "1.99€",
+                id: 3,
+            },
+        ],
+        ingridients: [{
+            ingridient1 : "Ingridient 1",
+            ingridient2 : "Ingridient 2",
+            ingridient3 : "Ingridient 3",
+        }]
+    },
+    ]
+    export default productData
