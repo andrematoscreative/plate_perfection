5590f43c4e9de7e11a93889e4c90e9e05aa78fe1
Moodyean <andrefnmatos@gmail.com>
2023-02-16 12:37:18 +0000

    teste_contact

--- plate_perfection/src/container/Contact/Contact.jsx
+++ plate_perfection/src/container/Contact/Contact.jsx
@@ -5,29 +5,31 @@ import { images } from '../../constants';
 import contact from './contact.scss';
 
 const Contact = () => (
-  <div>
+  <div className="app__contact">
     <motion.div
       whileInView={{ x: [-200, 0], opacity: [0, 1] }}
       transition={{ duration: 2 }}
-      className="app__contact app__flex"
     >
-      <div className="app__contact-container app__flex">
+      <div className="app__contact-container">
 
         <div className='app__contact-title'>
-          <img src={images.plate_perfection_logo} alt="plate_perfection_logo" />
+          <img  className='plate_perfection_logo' src={images.plate_perfection_logo} alt="plate_perfection_logo" />
           <h1>Contact</h1>
+          <div className='line__contact-title'/>
         </div>
 
-          <div className='line__contact-title'/>
-
       </div>
-      <img src={images.plate_perfection_logo_background} alt="plate_perfection_logo_background" />
+      
       <div className='app__contact-components'>
-      <h1>Czgfjhsxgjxygjsxyjsjjsontact</h1>
+      
+      <h1>Plate Perfection</h1>
+      <div className='line2'/>
+      
       </div>
     </motion.div>
-
+    
+    <img className='plate_perfection_logo_background' src={images.plate_perfection_logo_background} alt="plate_perfection_logo_background" />
+  
   </div>
-
 );
 export default AppWrap(Contact, 'Contact');
