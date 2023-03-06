4773063f79d2fddee7e673e92ff2be92302d52c8
Moodyean <andrefnmatos@gmail.com>
2023-03-06 23:13:03 +0000

    header final

--- plate_perfection/src/container/Home/Home.jsx
+++ plate_perfection/src/container/Home/Home.jsx
@@ -7,29 +7,31 @@ import './home.scss';
 
 
 const Home = () => (
-  <div  className='app__master-container'>
-    <div className="app__home app__flex">
-      <motion.div
-      whileInView={{ y: [-200, 0], opacity: [0, 1] }}
-      transition={{ duration: 2 }}
-      className='app__home_title'>
-          <h1 className="head-text">Plate Perfection</h1>
-          <div className='line1'/>
-      </motion.div>
+  <div className='app__home_backgimg'>
+    <div  className='app__master-container'>
+      <div className="app__home app__flex">
+        <motion.div
+        whileInView={{ y: [-200, 0], opacity: [0, 1] }}
+        transition={{ duration: 2 }}
+        className='app__home_title'>
+            <h1 className="head-text">Plate Perfection</h1>
+            <div className='line1'/>
+        </motion.div>
 
-      <motion.div
-      whileInView={{ x: [200, 0], opacity: [0, 1] }}
-      transition={{ duration: 2.5 }}
-      className="app__home-text app__flex">
-          <p className="p-text app__flex">"Savor every bite of impeccable cuisine, crafted to perfection with the finest ingredients. Experience the art of dining at Plate Perfection, where every dish is a masterpiece waiting to be savored."</p>
-      </motion.div>
-      
-      <motion.div
-      whileInView={{ scale: [0, 1]  }}
-      transition={{ duration: 2.5 }}
-      className='app__home-line'>
-      </motion.div>
-    
+        <motion.div
+        whileInView={{ x: [200, 0], opacity: [0, 1] }}
+        transition={{ duration: 2.5 }}
+        className="app__home-text app__flex">
+            <p className="p-text app__flex">"Savor every bite of impeccable cuisine, crafted to perfection with the finest ingredients. Experience the art of dining at Plate Perfection, where every dish is a masterpiece waiting to be savored."</p>
+        </motion.div>
+        
+        <motion.div
+        whileInView={{ scale: [0, 1]  }}
+        transition={{ duration: 2.5 }}
+        className='app__home-line'>
+        </motion.div>
+      </div>
+    </div>
       <motion.img
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 2, ease: 'easeInOut' }}
@@ -37,7 +39,6 @@ const Home = () => (
         alt="profile_plate_perfection_home_background"
         className="app__home-img"
       />
-    </div>
   </div>
 );
 
