0c2a74efb8819b9816b873e4c5801a02fe23a751
Moodyean <andrefnmatos@gmail.com>
2023-03-26 02:24:07 +0100

    test4

--- plate_perfection/src/container/Productdetails/Productdetails.jsx
+++ plate_perfection/src/container/Productdetails/Productdetails.jsx
@@ -43,7 +43,7 @@ const Productdetails = ({ card, onClose, item }) => {
   const addExtras = (item) => {
     extras.push({ name: item.name, price: item.price, quantity: item.quantity +1 });
     setExtraAdded(true)
-    setTimeout(() => setExtraAdded(false), 700);
+    setTimeout(() => setExtraAdded(false), 1000);
   };
   
   
@@ -191,107 +191,3 @@ const Productdetails = ({ card, onClose, item }) => {
   };
 
 export default AppWrap(Productdetails, 'Productdetails');
-
-
-
-/* import React, { useParams } from "react-router-dom";
-import { useState, useEffect, useContext } from "react";
-import { motion } from 'framer-motion';
-import { AppWrap } from '../../wrapper';
-import { AiFillTwitterCircle, AiFillFacebook} from "react-icons/ai";
-import { images } from '../../constants';
-import "./productdetails.scss";
-import { productData } from '..';
-
-const Productdetails = ({ card, onClose, item }) => {
-  const { id } = useParams();
-  let product = null 
-  
-  const AddToCart = (item) => {console.log('Item added to cart:', item);};
-  
-  
-  if (id !== undefined) {
-    product = productData[parseInt(id) - 1];
-  }
-  
-  return (
-      <div className="app__menu-card-details">
-        <div className="app__master-container">
-
-          <div className="app__menu-card-details-header">
-            <motion.div  
-            whileInView={{ y: [-35, 0], opacity: [0, 1] }}
-            transition={{ duration: 2 }} 
-            className='app__menu-card-details-header-title'>
-              <img className="app__menu-card-details-header-title-logo" src={images.plate_perfection_logo} alt="plate_perfection_logo" />
-              <h1 className='app__menu-card-details-header-title-title'>Product Detail</h1>
-              <motion.div
-                whileInView={{ scale: [0, 1]  }}
-                transition={{ duration: 3 }}
-                className='app__menu-card-details-header-title-line'>
-              </motion.div>
-            </motion.div>
-          </div>
-
-          <div className="app__menu-card-details-container">
-            
-            <div className="app__menu-card-details-image-container">
-              <motion.div whileInView={{opacity: [0, 1] }} transition={{ duration: 3 }}>
-                <img src={product.image} alt={product.name} className="app__menu-card-details-image-container-image" />
-              </motion.div>
-            </div>
-
-            <div className="app__menu-card-details-info-container">
-              <div className="app__menu-card-details-info-container-name">{product.name}
-                <motion.div
-                  whileInView={{ x: [50, 0]}}
-                  transition={{ duration: 2.5 }}
-                  className="app__menu-card-details-info-container-name-line">
-                </motion.div>
-              </div>
-                <p className="app__menu-card-details-info-container-description">{product.description}</p>
-                <p className="app__menu-card-details-info-container-body">{product.body}</p>
-                <p className="app__menu-card-details-info-container-price">{product.price}</p> 
-                
-                {product.Ingredients.map((ing) => (
-                <div className="app__menu-card-details-info-container-ingredients"
-                  key={product.id}
-                >
-                  <p>{ing.Ingredient1}</p>
-                  <p>{ing.Ingredient2}</p>
-                  <p>{ing.Ingredient3}</p>
-                </div>
-                ))}
-
-                <button className="app__menu-card-details-info-container-add__to-cart-button" onClick={() => AddToCart(item)}>Add item to cart</button>
-                
-            </div>
-              
-              <motion.div
-                  whileInView={{scale: [0, 1]}}
-                  transition={{ duration: 2 }}
-                  className='app__menu-card-details-downline'>
-              </motion.div>
-              
-            <div className="app__menu-card-details-share-container">
-              
-              <p className="app__menu-card-details-share-container-title">Share your perfect moment</p>
-              <div className="app__menu-card-details-share-container-icons">
-                <a onClick={() => window.open("https://www.twitter.com", "Twitter", "width=600,height=400")}>
-                <AiFillTwitterCircle />
-                </a>
-                <a onClick={() => window.open("https://www.facebook.com", "Facebook", "width=600,height=400")}>
-                <AiFillFacebook />
-                </a>
-              </div>
-
-            </div>
-          </div>
-
-        </div>
-      </div>
-    
-    );
-  };
-
-export default AppWrap(Productdetails, 'Productdetails'); */
